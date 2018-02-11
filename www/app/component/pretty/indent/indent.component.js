f8a240b58c6a6c088f5d31433d4b6a7c70cf68b4
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-02-11 17:46:43 +0000

    Re-enable AnnoC/AnnoE rendering

--- www/app/component/pretty/indent/indent.component.js
+++ www/app/component/pretty/indent/indent.component.js
@@ -1,6 +1,6 @@
 module.exports = {
 	template: `
-		<span class="d-inline-block pl-3 indent" ng-click="$ctrl.toggle(); $event.stopPropagation()">
+		<span class="d-inline-block indent" ng-class="{'pl2': !$ctrl.collapsed}" ng-click="$ctrl.toggle(); $event.stopPropagation()">
 			<span class="indent-collapse clickable" ng-show="$ctrl.collapsed">(..)</span>
 			<ng-transclude ng-show="!$ctrl.collapsed">
 		</span>`,
