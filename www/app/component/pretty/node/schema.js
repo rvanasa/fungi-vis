fc2153c0e23bcdbfd3487ad298cbbabbf74d9bb2
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-02-19 03:58:51 +0000

    Minor tweaks/fixes

--- www/app/component/pretty/node/schema.js
+++ www/app/component/pretty/node/schema.js
@@ -3,7 +3,7 @@ module.exports = function(type)
 	return {
 		template: `
 			<p-handle type="${type}"
-				ng-mouseenter="$ctrl.focus(); $event.stopPropagation()"
+				ng-mouseover="$ctrl.focus(); $event.stopPropagation()"
 				ng-mouseout="$ctrl.unfocus(); $event.stopPropagation()"
 				ng-mousedown="$ctrl.select() && $event.stopPropagation()"
 			/>`,
