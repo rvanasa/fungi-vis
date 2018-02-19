26e6deed6adb5f29be211bc4a9c1c0496c041f7d
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-02-19 00:17:52 +0000

    Color scheme revamp, Phase I

--- www/app/component/pretty/indent/indent.component.js
+++ www/app/component/pretty/indent/indent.component.js
@@ -3,15 +3,11 @@ module.exports = {
 		<span class="d-inline-block indent" ng-class="{'pl-3': !$ctrl.collapsed}"
 			ng-click="$ctrl.collapsed = false; $event.stopPropagation()"
 			ng-dblclick="$ctrl.collapsed = true; $event.stopPropagation()">
-			<span class="indent-collapse clickable" ng-show="$ctrl.collapsed">(..)</span>
+			<span class="text-warning indent-collapse clickable" ng-show="$ctrl.collapsed">(..)</span>
 			<ng-transclude ng-show="!$ctrl.collapsed">
 		</span>`,
 	transclude: true,
 	bindings: {
 		collapsed: '<',
-	},
-	controller: function()
-	{
-		var $ctrl = this;
 	}
 }
