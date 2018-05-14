262fda6df2709bf7a6997eac12388553a3ebff29
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-05-14 18:18:36 +0000

    Further improve deploy commit logic

--- util/deploy.sh
+++ util/deploy.sh
@@ -6,19 +6,21 @@ heroku git:remote --app fungi-lang &&
 
 node_modules/webpack/bin/webpack.js --bail --progress --profile &&
 
-# TODO run tests
+cd repo/fungi-lang.rust &&
+cargo test &&
+cd ../.. &&
+
 cp -r ./repo/fungi-lang.rust/target ./dist/examples
 rm -rf ./dist/examples/debug
 rm ./dist/examples/rustc_info.json
 
 git add . &&
-git commit --amend --no-edit &&
+git commit -m "(Deploy)" &&
 git push heroku master --force
 
 rm -rf dist
 
-git add .
-git commit --amend --no-edit
+git reset HEAD~1
 
 ##  Helpful for testing:  ##
 # git add . && git commit --amend --no-edit && git push heroku master --force && heroku logs -t
