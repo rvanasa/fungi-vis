2b59775cd532a019253fc68e7ced91bb3140fa83
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-05-14 17:09:53 +0000

    Update deploy script to use relative webpack path

--- util/deploy.sh
+++ util/deploy.sh
@@ -2,7 +2,7 @@ cd ..
 
 rm -rf dist &&
 
-webpack --bail --progress --profile &&
+node_modules/webpack/bin/webpack.js --bail --progress --profile &&
 
 # TODO run tests
 cp -r ./repo/fungi-lang.rust/target ./dist/examples
@@ -10,14 +10,13 @@ rm -rf ./dist/examples/debug
 rm ./dist/examples/rustc_info.json
 
 git add . &&
-# git commit -m"(Deploy)"
-# git push origin master
-# git push heroku master
 git commit --amend --no-edit &&
-git push heroku master --force &&
+git push heroku master --force
 
 rm -rf dist
 
+git add .
+git commit --amend --no-edit
 
 ## Helpful for testing ##
 # git add . && git commit --amend --no-edit && git push heroku master --force && heroku logs -t
